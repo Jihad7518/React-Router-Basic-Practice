b97c55c99c1d9476faff23cff4a430f7f5d7e90c
Md. Jihad <125210346+Jihad7518@users.noreply.github.com>
2023-09-20 02:09:41 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,14 @@
 
+import { NavLink, Route, Routes } from 'react-router-dom';
+import './App.css';
+import Home from './components/Home';
+import NotFound from './components/NotFound';
+import Labs from './components/Labs';
+import About from './components/About';
+import Support from './components/Support';
+import MainHeader from './components/MainHeader';
+import { Link } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
