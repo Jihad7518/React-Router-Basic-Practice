5ee9e833c791ed29d9929b997597f9266679c6e0
Md. Jihad <125210346+Jihad7518@users.noreply.github.com>
2023-09-21 21:13:01 +0600

    Update Labs.js

--- src/components/Labs.js
+++ src/components/Labs.js
@@ -1,1 +1,27 @@
 
+
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
+
+const Labs = () => {
+
+  const navigate = useNavigate();
+
+  function clickHandler() {
+    navigate("/about");
+  }
+  return (
+    <div>
+        <div>
+          This is Labs Page
+        </div>
+        
+        <div>
+          <button onClick={clickHandler}>Move to About Page</button>
+        </div>
+
+    </div>
+  )
+}
+
+export default Labs
