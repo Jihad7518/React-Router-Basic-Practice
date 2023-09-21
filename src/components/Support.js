963d3f1bf07ff3f38fff96e753e275c330b6e744
Md. Jihad <125210346+Jihad7518@users.noreply.github.com>
2023-09-21 21:13:51 +0600

    Update Support.js

--- src/components/Support.js
+++ src/components/Support.js
@@ -1,1 +1,16 @@
 
+import React from 'react'
+import { useNavigate } from 'react-router-dom';
+
+const Support = () => {
+
+  const navigate = useNavigate();
+
+  function clickHandler() {
+    navigate("/labs");
+  }
+
+  function backHandler() {
+    navigate(-1);
+  }
+
