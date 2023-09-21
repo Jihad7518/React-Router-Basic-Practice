b3a120d63743bd83cfb53091ff56105787668850
Md. Jihad <125210346+Jihad7518@users.noreply.github.com>
2023-09-21 21:14:01 +0600

    Update Support.js

--- src/components/Support.js
+++ src/components/Support.js
@@ -14,3 +14,19 @@ const Support = () => {
     navigate(-1);
   }
 
+  return (
+    <div>
+        <div>
+          This is Support Page
+        </div>
+        
+        <div>
+          <button onClick={clickHandler}>Move to Support Page</button>
+          <button onClick={backHandler}>Go Back</button>
+        </div>
+
+    </div>
+  )
+}
+
+export default Support
